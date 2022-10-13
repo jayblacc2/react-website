44d1485a240299bbb92b46af509ad49da677a45a
jayblacc2 <jideman2k@yandex.ru>
2022-10-13 07:49:30 +0400

    About us page

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import AboutUs from './pages/AboutUs';
+function App() {
+  return (
+    <div className="App">
+      <AboutUs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AboutSection from '../components/AboutSection';
+
+export default function AboutUs() {
+  return (
+    <div>
+      <AboutSection />
+    </div>
+  );
+}
