cea0c9668d12e094d8f03856e9979db710f5b6f0
jayblacc2 <jideman2k@yandex.ru>
2022-10-13 22:00:29 +0400

    Intro styled Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import GlobalStyle from './components/GlobslStyle';
 import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
       <AboutUs />
+      <GlobalStyle />
     </div>
   );
 }
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,32 +1,66 @@
 import React from 'react';
 import home1 from '../imgs/home1.png';
+import styled from 'styled-components';
 
 export default function AboutSection() {
   return (
-    <div className="About">
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span> dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contact us for any photography or videography ideas you've got. We are
           professionals with amazing skills
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image className="image">
         <img src={home1} alt="Camera guy" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 }
+
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: #fff;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
