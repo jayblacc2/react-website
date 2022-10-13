3052fcc7decd39b03e3e1df94f3d51936d36b0c7
jayblacc2 <jideman2k@yandex.ru>
2022-10-14 00:43:26 +0400

    styled services section

--- src/components/ServiceSection.js
+++ src/components/ServiceSection.js
@@ -4,46 +4,79 @@ import diaphragm from '../imgs/diaphragm.svg';
 import money from '../imgs/money.svg';
 import teamwork from '../imgs/teamwork.svg';
 import home2 from '../imgs/home2.png';
+import styled from 'styled-components';
+import {About, Description, Image, Hide} from '../styles';
 
 export default function ServiceSection() {
   return (
-    <div className="Services">
-      <h2>
-        High <span>quality</span> services
-      </h2>
-      <div className="cards">
-        <div className="card">
-          <div className="icon">
-            <img src={clock} alt="?" />
-            <h3>Efficient</h3>
-          </div>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-        <div className="card">
-          <div className="icon">
-            <img src={diaphragm} alt="?" />
-            <h3>Efficient</h3>
-          </div>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-        <div className="card">
-          <div className="icon">
-            <img src={money} alt="?" />
-            <h3>Efficient</h3>
-          </div>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-        <div className="card">
-          <div className="icon">
-            <img src={teamwork} alt="?" />
-            <h3>Efficient</h3>
-          </div>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-      </div>
-      <div className="image">
+    <Service>
+      <Description>
+        <h2>
+          High <span>quality</span> services
+        </h2>
+        <Cards>
+          <Card>
+            <div className="icon">
+              <img src={clock} alt="?" />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={diaphragm} alt="?" />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={money} alt="?" />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={teamwork} alt="?" />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+        </Cards>
+      </Description>
+      <Image className="image">
         <img src={home2} alt="camera" />
-      </div>
-    </div>
+      </Image>
+    </Service>
   );
 }
+
+const Service = styled(About)`
+  h3 {
+    padding-bottom: 5rem;
+  }
+
+  p {
+    width: 70%;
+    padding: 2rem 0 4rem 0;
+  }
+`;
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Card = styled.div`
+  flex-basis: 15rem;
+
+  .icon {
+    display: flex;
+    align-items: center;
+    h3 {
+      margin-left: 1rem;
+      background: #fff;
+      color: #000;
+      padding: 1rem;
+    }
+  }
+`;
---
+++ src/styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+export const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: #fff;
+`;
+
+export const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+export const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+export const Hide = styled.div`
+  overflow: hidden;
+`;
+
+//TODO style component globalStyle
+//styled Component local style
+//styled Component reuseable style
+//customize style comp style(NameCompStyle)
